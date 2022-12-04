f983810b1642fa03c78fc245996dc64cb5d3506b
César Alva <cesar_88alva@hotmail.com>
2022-12-03 20:20:51 -0500

    Intellij Commit

--- operation/src/main/java/com/bootcamp/project/operation/OperationApplication.java
+++ operation/src/main/java/com/bootcamp/project/operation/OperationApplication.java
@@ -2,8 +2,10 @@ package com.bootcamp.project.operation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class OperationApplication {
 
 	public static void main(String[] args) {
