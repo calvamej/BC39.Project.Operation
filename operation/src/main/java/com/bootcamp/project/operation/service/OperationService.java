72c86b5991008f570d4063f51f70c7177f7380d1
César Alva <cesar_88alva@hotmail.com>
2022-12-16 10:52:17 -0500

    WebTestClient

--- operation/src/main/java/com/bootcamp/project/operation/service/OperationService.java
+++ operation/src/main/java/com/bootcamp/project/operation/service/OperationService.java
@@ -13,14 +13,12 @@ public interface OperationService {
 
     public Flux<OperationEntity> getAll();
     public Mono<OperationEntity> getOne(String accountNumber);
-
     public Mono<OperationEntity> save(OperationEntity colEnt);
     public Mono<OperationEntity> update(String accountNumber, double amount);
     public Mono<Void> delete(String accountNumber);
     public Flux<OperationEntity> getByClientAndProduct(String clientDocumentNumber, String productCode);
     public Flux<OperationEntity> getByAccount(String accountNumber);
     public Flux<OperationEntity> getByCredit(String creditNumber);
-
     public Flux<OperationReportEntity> getCommissionsByProduct(Date initialDate, Date finalDate);
     public Flux<OperationEntity> getLast10ByDebitCard(String debitCardNumber);
     public Flux<OperationEntity> getLast10ByCreditCard(String creditCardNumber);
